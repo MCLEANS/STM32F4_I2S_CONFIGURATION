b8e470db4546794f7f104d5eb1e1b1f61bd1a66a
MCLEANS <jackagwaya@gmail.com>
2021-03-18 08:09:07 +0300

    test changes

--- main.cpp
+++ main.cpp
@@ -7,8 +7,21 @@ int main(void) {
   
   system_clock.initialize();
 
+  /* Enable clock port RCC */
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+
+  /* Enable SPI RCC */
+  RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
+
+  /* Set clock pin to output Alternate Function */
+  GPIOB->MODER &= ~GPIO_MODER_MODER10;
+  GPIOB->MODER |= GPIO_MODER_MODER10_1;
+
+  /* Select specific alternate function */
+  GPIOB->AFR[1] |= (5<<8);
+
   /* Enable PLLI2S */
-  RCC->CR |= (1 << RCC_CR_PLLI2SON);
+  RCC->CR |= RCC_CR_PLLI2SON;
 
   /* Wait until PLLI2S is Enabled */
   while(!(RCC->CR & RCC_CR_PLLI2SRDY)){}
@@ -26,14 +39,37 @@ int main(void) {
   /* Set PLLI2SN value to 258 */
   RCC->PLLI2SCFGR &= ~RCC_PLLI2SCFGR_PLLI2SN;
   RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SN_1;
+  RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SN_2;
+  RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SN_3;
+  RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SN_5;
   RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SN_8;
 
-  /* Set PLLI2SR to # */
+  /* Set PLLI2SR to 3 */
   RCC->PLLI2SCFGR &= ~RCC_PLLI2SCFGR_PLLI2SR;
   RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SR_0;
   RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SR_1;
+  RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SR_2;
 
-  
+  /* Set the I2S Linear prescaler with a Value of 42 */
+  SPI2->I2SPR &= ~SPI_I2SPR_I2SDIV;
+  SPI2->I2SPR |= 220;
+
+  /* Set odd factor for the I2S prescaler */
+  SPI2->I2SPR &= ~SPI_I2SPR_ODD;
+
+  /* Enable I2S mode */
+  SPI2->I2SCFGR |= SPI_I2SCFGR_I2SMOD;
+
+  /* Set I2S to master receive mode */
+  SPI2->I2SCFGR |= SPI_I2SCFGR_I2SCFG;
+
+ // SPI2->I2SPR |= SPI_I2SPR_MCKOE;
+
+  SPI2->I2SCFGR |= SPI_I2SCFGR_I2SSTD_1;
+
+  /* Enable SPI */
+  SPI2->I2SCFGR |= SPI_I2SCFGR_I2SE;
+
 
 
 
