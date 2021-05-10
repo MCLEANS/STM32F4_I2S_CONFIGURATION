9bbf193efea31a8a2b9294c6b528e56b121423b2
MCLEANS <jackagwaya@gmail.com>
2021-05-10 16:37:43 +0300

    Fixed clock configuration

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 custom_libraries::clock_config system_clock;
 
+#define DFREQ 12000
+#define FS DFREQ
+#define DECIMATION 64
+#define I2S_FS ((FS*DECIMATION)/(16*2))
+
 int main(void) {
   
   system_clock.initialize();
@@ -17,6 +22,10 @@ int main(void) {
   GPIOB->MODER &= ~GPIO_MODER_MODER10;
   GPIOB->MODER |= GPIO_MODER_MODER10_1;
 
+  /* Set GPIO SPEED to veery high */
+  GPIOB->OSPEEDR &= ~GPIO_OSPEEDER_OSPEEDR10;
+  GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR10;
+
   /* Select specific alternate function */
   GPIOB->AFR[1] |= (5<<8);
 
@@ -30,12 +39,12 @@ int main(void) {
    * PLL I2S Clock output frequency is set at 86MHz
    */
   
-  /* Set PLLI2SN value to 429 */
+  /* Set PLLI2SN value to 258 */
   RCC->PLLI2SCFGR &= ~RCC_PLLI2SCFGR_PLLI2SN;
-  RCC->PLLI2SCFGR |= (258 < 6);
+  RCC->PLLI2SCFGR |= (258 << 6);
   
 
-  /* Set PLLI2SR to 4 */
+  /* Set PLLI2SR to 3 */
   RCC->PLLI2SCFGR &= ~RCC_PLLI2SCFGR_PLLI2SR;
   RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SR_0;
   RCC->PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SR_1;
@@ -48,7 +57,7 @@ int main(void) {
 
   /* Set the I2S Linear prescaler with a Value of 42 */
   SPI2->I2SPR &= ~SPI_I2SPR_I2SDIV;
-  SPI2->I2SPR |= 3;
+  SPI2->I2SPR |= 17;
 
   /* Set odd factor for the I2S prescaler */
   SPI2->I2SPR &= ~SPI_I2SPR_ODD;
@@ -59,13 +68,13 @@ int main(void) {
   /* Set I2S to master receive mode */
   SPI2->I2SCFGR |= SPI_I2SCFGR_I2SCFG;
 
-  SPI2->I2SPR |= SPI_I2SPR_MCKOE;
 
-  SPI2->I2SCFGR &= ~SPI_I2SCFGR_I2SSTD;
-
-  //SPI2->I2SCFGR |= SPI_I2SCFGR_DATLEN_0;
+  SPI2->I2SCFGR |= SPI_I2SCFGR_I2SSTD_1;
 
   SPI2->I2SCFGR |= SPI_I2SCFGR_CKPOL;
+
+  SPI2->I2SCFGR &= ~SPI_I2SCFGR_DATLEN;
+
   /* Enable SPI */
   SPI2->I2SCFGR |= SPI_I2SCFGR_I2SE;
 
